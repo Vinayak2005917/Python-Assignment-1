82254968b659b0beea14621bc5f2e28fe3596a8b
Vinayak2005917 <145529911+Vinayak2005917@users.noreply.github.com>
2023-12-01 11:28:19 +0530

    Create Min of two.py

---
+++ Min of two.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if a<b:
+    print(f"the smaller number is {a}")
+else:
+    print(f"the smaller number is {b}")
