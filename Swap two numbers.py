46b2c572e0164f8865a1c0e471616ad6b136ec51
Vinayak2005917 <145529911+Vinayak2005917@users.noreply.github.com>
2023-12-01 11:29:55 +0530

    Create Swap two numbers.py

---
+++ Swap two numbers.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the First number: "))
+num2 = int(input("Enter the second number: "))
+temp = num1
+num1 = num2
+num2 = temp
+print(f"After the swap value of the first number is {num1} and the value of the second number is {num2}")
